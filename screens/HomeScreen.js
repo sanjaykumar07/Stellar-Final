b812d4361df6241a03b21ff35267f1530a45905b
Sanjay Kumar <73998919+sanjaykumar07@users.noreply.github.com>
2021-08-12 18:13:34 +0530

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,7 +14,7 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Text>Win pannita marra! Home Screen</Text>
+        <Text>Home Screen</Text>
       </View>
     );
   }
